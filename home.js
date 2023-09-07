71531108d60f925ed7c534e0b73237cfa180b665
Yey007 <yey007tech@gmail.com>
2023-09-07 14:26:44 -0400

    Played with colors

--- home.js
+++ home.js
@@ -1,14 +1,14 @@
 const allWords = document.getElementById("roll-container").children;
-let lastRolledOutIndex = 0;
+let lastRolledOutIndex = allWords.length - 1; // last element
 
 function next() {
   const toHide = allWords.item(lastRolledOutIndex);
   const toRollOut = allWords.item((lastRolledOutIndex + 1) % allWords.length);
   const toRollIn = allWords.item((lastRolledOutIndex + 2) % allWords.length);
 
-  toHide.classList.replace("roll-out", "hidden");
-  toRollOut.classList.replace("roll-in", "roll-out");
-  toRollIn.classList.replace("hidden", "roll-in");
+  toHide.classList.replace("rolled-out", "hidden");
+  toRollOut.classList.replace("rolled-in", "rolled-out");
+  toRollIn.classList.replace("hidden", "rolled-in");
 
   lastRolledOutIndex = (lastRolledOutIndex + 1) % allWords.length;
 }
